d1b90a9b50c56f9384d78d19f94eed2275baabf6
renanp96 <renanpa1996@gmail.com>
2026-04-09 12:09:43 -0300

    finished basic behavior of login-service

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -3,13 +3,17 @@ import { DefaultLoginLayout } from '../../components/default-login-layout/defaul
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PrimaryInput } from '../../components/primary-input/primary-input';
 import { Router } from '@angular/router';
+import { LoginService } from '../../services/login-service';
 
 @Component({
   selector: 'app-login',
   imports: [
     DefaultLoginLayout,
     ReactiveFormsModule,
-    PrimaryInput
+    PrimaryInput,
+  ],
+  providers: [
+    LoginService
   ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
@@ -18,7 +22,8 @@ export class Login {
   loginForm!: FormGroup;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private loginService: LoginService
   ) {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
@@ -27,7 +32,15 @@ export class Login {
   }
 
   submit() {
-    console.log(this.loginForm.value);
+    let email, password;
+
+    email = this.loginForm.value.email;
+    password = this.loginForm.value.password;
+
+    this.loginService.login(email, password).subscribe({
+      next: () => console.log("Sucesso!"),
+      error: () => console.log("Erro!")
+    });
   }
 
   navigate() {
